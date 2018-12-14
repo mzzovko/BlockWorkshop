5d5635284de3a4f0d8d4898596a30610539141db
Marko Zovko <mazovko@foi.hr>
2018-12-14 15:56:57 +0100

    Step 7

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,20 +1,28 @@
 pragma solidity ^0.4.20;
+
 import "zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import './Metadata.sol';
 
 /**
  * The Token contract does this and that...
  */
-contract Token is ERC721Token {
+contract Token is ERC721Token, Ownable {
     address metadata;
     constructor(string name, string symbol, address _metadata) public
     ERC721Token(name, symbol)
     {
         metadata = _metadata;
     }
+
     function getMetadata() public view returns (address) {
         return metadata;
     }
+
+    function updateMetadata(address _metadata) public onlyOwner {
+        metadata = _metadata;
+    }
+
     function tokenURI(uint _tokenId) public view returns (string _infoUrl) {
         address _impl = getMetadata();
         bytes memory data = msg.data;
@@ -24,8 +32,8 @@ contract Token is ERC721Token {
             let ptr := mload(0x40)
             returndatacopy(ptr, 0, size)
             switch result
-            case 0 { revert(ptr, size) }
-            default { return(ptr, size) }
+            case 0 {revert(ptr, size)}
+            default {return (ptr, size)}
         }
     }
 }
